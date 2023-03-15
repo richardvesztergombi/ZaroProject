6a740a6ba4cb772204b26f526274c9d7d90310b9
Richard Vesztergombi <75344446+richardvesztergombi@users.noreply.github.com>
2023-03-15 16:07:04 +0100

    First commit

---
+++ src/main/java/com/example/ZaroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaroProjectApplication.class, args);
+	}
+
+}
