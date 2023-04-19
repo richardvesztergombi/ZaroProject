81c609b0ba910029aca94f759db851847d7cbe6a
richardvesztergombi <richard.vesztergombi@gmail.com>
2023-04-19 11:48:24 +0200

    Just developed some JUnit test

---
+++ src/main/java/com/example/tests/UserDTOTest.java
@@ -0,0 +1,22 @@
+/*import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class UserDTOTest{
+
+    @Test
+    public void testGetSetUserName() {
+        UserDTO user = new UserDTO();
+        String userName = "johnDoe";
+        user.setUserName(userName);
+        assertEquals(userName, user.getUserName());
+    }
+
+    @Test
+    public void testGetSetPassword() {
+        UserDTO user = new UserDTO();
+        String password = "password123";
+        user.setPassword(password);
+        assertEquals(password, user.getPassword());
+    }
+
+}*/
